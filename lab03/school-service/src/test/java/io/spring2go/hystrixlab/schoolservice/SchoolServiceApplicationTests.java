1e40e41feb5fd931ecf1af6d4ac25d10476bcc7a
dwyanwede <shang_sk@163.com>
2019-04-12 14:50:09 +0800

    滑动窗口协议在RxJava中的实现

--- lab03/school-service/src/test/java/io/spring2go/hystrixlab/schoolservice/SchoolServiceApplicationTests.java
+++ lab03/school-service/src/test/java/io/spring2go/hystrixlab/schoolservice/SchoolServiceApplicationTests.java
@@ -1,16 +1,31 @@
 package io.spring2go.hystrixlab.schoolservice;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.lang.math.RandomUtils;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import rx.Observable;
+import rx.internal.util.InternalObservableUtils;
+import java.util.concurrent.TimeUnit;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class SchoolServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	}
+
+	// 滑动窗口协议在RxJava中的实现
+	@Test
+	public void timeWindowTest() throws Exception{
+		Observable<Integer> source = Observable.interval(50, TimeUnit.MILLISECONDS).map(i -> RandomUtils.nextInt(2));
+		source.window(1, TimeUnit.SECONDS).subscribe(window -> {
+			int[] metrics = new int[2];
+			window.subscribe(i -> metrics[i]++,
+					InternalObservableUtils.ERROR_NOT_IMPLEMENTED,
+					() -> System.out.println("窗口Metrics:" + JSON.toJSONString(metrics)));
+		});
+		TimeUnit.SECONDS.sleep(5);
 	}
 
 }
